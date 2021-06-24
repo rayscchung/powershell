573ab0d7b81bc2f986bfbc4e400cfa3389c855bf
Raymond Chung <rayscchung@gmail.com>
2021-06-24 07:11:10 -0700

    adding files

---
+++ DropBoxIconPrioritizer.ps1
@@ -0,0 +1,18 @@
+<#
+Currently, Windows has a limit of 15 overlay icons that can be loaded.DESCRIPTION
+With the installation of One Drive, Windows prioritizes its own icons over DropBox's
+This has caused Dropbox Overlay icons to break and many Marketplace Users are complaining about this.
+This script finds all the DropBox Icon Overlay Identifier Registry Items and renames them with spaces in front so that
+they will be prioritized over the One Drive Icons.
+#>
+
+$path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ShellIconOverlayIdentifiers"
+$keys = Get-ChildItem $path
+
+foreach($key in $keys) {
+  if( $key -like "*DropBox*" ) {
+    $itemname = $key.name.split("\")[7] -replace "[ ]",""
+    $pathname = "HKLM:\$($key.name.trim("HKEY_LOCAL_MACHINE\"))"
+    #Rename-Item -path $pathname -NewName "          $($itemname)"
+  }
+}
